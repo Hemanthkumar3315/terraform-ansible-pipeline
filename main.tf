9e7c60b9b632c36dd4a5f6ae263cfcddc08cf991
Hemanthkumar3315 <hemanthkumar.chikkala26@gmail.com>
2025-11-13 12:06:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,11 +22,17 @@ terraform {
 # Provider Configuration
 ##########################################
 provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+  region                      = var.aws_region
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  s3_force_path_style         = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  endpoints {
+    s3 = "http://localhost:4566" # LocalStack endpoint
+  }
 }
-
-provider "random" {}
 
 ##########################################
 # Random ID for Unique Bucket Name
